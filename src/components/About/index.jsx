e69bee3aba5a6c07227c295326ecd3ea876145ab
glft-MypPfsmyKM_Xdmud1Lbu <madhushika.dissanayake@onsystechnologies.com>
2026-06-03 12:56:00 +0530

    updated changes

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -35,7 +35,7 @@ const About = () => {
                         </div>
                     </div>
                     <div className="about_info_bottom">
-                        <a href="/Madhushika_Dissanayake_ Resume.pdf" download className='about_btn'>Download CV</a>
+                        <a href="/Madhushika_Dissanayake_Resume.pdf" download className='about_btn'>Download CV</a>
                         <Link to="contact" spy={true} smooth={true} offset={-70} duration={500} className='about_btn'>
                             Contact Me
                         </Link>
